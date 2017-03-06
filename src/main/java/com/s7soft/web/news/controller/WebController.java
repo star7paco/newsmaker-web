b156803999b42ebd5d914d4416c9df3dfbbfcdd1
星 明換 <mhoshi@so-netmedia.jp>
2017-03-06 18:28:31 +0900

    spring 設定追加

--- src/main/java/com/s7soft/web/news/controller/WebController.java
+++ src/main/java/com/s7soft/web/news/controller/WebController.java
@@ -1,18 +1,18 @@
 package com.s7soft.web.news.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WebController{
 
-	@RequestMapping(value = "/top", method = RequestMethod.GET)
-    public String top() {
-		 ModelAndView view = new ModelAndView();
-		  view.setViewName("/top");
-		  view.addObject("welcomeMessage", "Welcome Message");
-        return "top";
+	@RequestMapping(value = "/showMessage.html", method = RequestMethod.GET)
+    public String showMessage(Model model) {
+		 model.addAttribute("message", "Welcome Message");
+		 System.out.println("call news showMessage.html");
+
+        return "showMessage";
     }
 }
